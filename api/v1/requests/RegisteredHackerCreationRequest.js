04118c3bc2e121da08d3bd0e78c0c7d25c9d5664
Jonathan Reynolds <j14reynolds@gmail.com>
2016-07-17 16:44:00 -0500

    Complete registration validations

--- api/v1/requests/RegisteredHackerCreationRequest.js
+++ api/v1/requests/RegisteredHackerCreationRequest.js
@@ -4,6 +4,22 @@ var registration = require('../utils/registration');
 
 var required = ['age', 'gender', 'school',  'graduationYear', 'major', 'diet', 'professionalInterest', 'linkedinUrl', 'githubUrl', 'siteUrl', 'hackathonAttendance', 'initiatives',  'hardwareDesired', 'openSourceInterests', 'extraInformation', 'tshirtSize'];
 var validations = {
+	'age': ['number', 'max:100', 'min:13']
+	'gender': ['string', registration.verifyGender],
+	'school': ['string', 'between:0:256'],
+	'graduationYear': ['number', 'max:2020', 'min:2017'],
+	'major': ['string', 'between:0:256'],
+	'diet': ['string', registration.verifyDiet],
+	'professionalInterest': ['string', registration.verifyProfessionalInterest],
+	'linkedinUrl': ['url', 'between:0:256'],
+	'githubUrl': ['url', 'between:0:256'],
+	'siteUrl': ['url', 'between:0:256'],
+  'hackathonAttendance': ['string', registration.verifyHackathonAttendance],
+  //Assuming initiatives will be passed as an array of strings
+  'initiatives': ['array', registration.verifyInitiatives],
+  'hardwareDesired': ['string', 'between:0:256'],
+  'openSourceInterests': ['string', 'between:0:256'],
+  'extraInformation': ['string', 'between:0:256'],
 	'tshirtSize': ['string', registration.verifyTshirtSize]
 };
 
--- api/v1/utils/registration.js
+++ api/v1/utils/registration.js
@@ -1,6 +1,11 @@
 var _ = require('lodash');
 
 var TSHIRT_SIZES = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+var GENDERS = ['NONE', 'MALE', 'FEMALE', 'NON_BINARY', 'OTHER'];
+var DIETS = ['NONE', 'VEGETARIAN', 'VEGAN', 'GLUTEN_FREE'];
+var PROFESSIONAL_INTERESTS = ['NONE', 'FULL_TIME', 'INTERNSHIP', 'BOTH'];
+var HACKATHON_ATTENDANCE_OPTIONS = ['0', '1-5', '5+'];
+var INITIATIVES = ['SOFTWARE', 'HARDWARE', 'OPEN_SOURCE'];
 
 /**
  * Ensures that the provided tshirt-size is in the list
@@ -15,4 +20,46 @@ module.exports.verifyTshirtSize = function (size) {
 	}
 
 	return true;
+};
+
+module.exports.verifyGender = function (gender) {
+ if (!_.includes(GENDERS, gender);) {
+   throw new TypeError(gender + " is not a valid gender");
+ }
+
+ return true;
+};
+
+module.exports.verifyDiet = function (diet) {
+ if (!_.includes(DIETS, diet);) {
+   throw new TypeError(diet + " is not a valid diet");
+ }
+
+ return true;
+};
+
+module.exports.verifyProfessionalInterest = function (interest) {
+ if (!_.includes(PROFESSIONAL_INTERESTS, interest);) {
+   throw new TypeError(interest + " is not a valid professional interest");
+ }
+
+ return true;
+};
+
+module.exports.verifyHackathonAttendance = function (attendance) {
+ if (!_.includes(HACKATHON_ATTENDANCE_OPTIONS, attendance);) {
+   throw new TypeError(attendance + " is not a valid attendance option");
+ }
+
+ return true;
+};
+
+
+module.exports.verifyInitiatives= function (initiatives) {
+  for(var initiative in initiatives){
+    if (!_.includes(INITIATIVES, initiative);) {
+     throw new TypeError(initiative + " is not a valid initiative");
+    }
+  }
+  return true;
 };
